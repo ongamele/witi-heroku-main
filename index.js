4d8093032bea1e97453897204e383bf544e0f7f7
ogeb@live.com <ogeb@live.com>
2023-10-08 17:53:41 +0200

    update

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const { ApolloServer } = require('apollo-server-express');
-const mongoose = require('mongoose');
+const { ApolloServer } = require("apollo-server-express");
+const mongoose = require("mongoose");
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const {
   GraphQLUpload,
   graphqlUploadExpress, // A Koa implementation is also exported.
-} = require('graphql-upload');
+} = require("graphql-upload");
 
 const PORT = process.env.PORT || 5000;
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require('./config');
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config");
 
 async function startServer() {
   const server = new ApolloServer({
@@ -23,7 +23,7 @@ async function startServer() {
   mongoose
     .connect(MONGODB, { useNewUrlParser: true })
     .then(() => {
-      console.log('MongoDB Connected');
+      console.log("MongoDB Connected");
       // return server.listen({ port: 5000 });
     })
     .then((res) => {
@@ -45,7 +45,7 @@ async function startServer() {
     res.send('This is the');
   });*/
 
-  await new Promise((r) => app.listen({ port: 5000 }, r));
+  await new Promise((r) => app.listen({ port: PORT }, r));
 
   console.log(`🚀 Server ready at http://localhost:5000${server.graphqlPath}`);
 
